a32ec6a56938804e9cb6384c0b96f0298d280fd0
Dương Hà Tuấn <hatuan1423@gmail.com>
2024-08-21 22:58:23 +0700

    2

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -10,7 +10,10 @@ const MaxWidthWrapper = ({
 }) => {
   return (
     <div
-      className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20")}
+      className={cn(
+        "h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
     >
       {children}
     </div>
